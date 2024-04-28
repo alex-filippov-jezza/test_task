6eb0ad52fb7aa3246330976e126dd6c6d95e9175
Alex Filippov <alex2000sti@gmail.com>
2024-04-28 17:06:04 +0300

    object for Check ckass

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ from classes.checkClass import Check
 
 
 file_path = 'чеки.txt.'
-
-# Создаем экземпляр класса DataProcessor
 checks = Check(file_path)
-
-# Обрабатываем данные
 df = checks.get_data()
 
-# Выводим DataFrame
 print(df)
