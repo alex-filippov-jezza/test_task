fbe58462f75fae246e9935d37cee4a24da5acc61
Alex Filippov <alex2000sti@gmail.com>
2024-04-28 18:17:39 +0300

    added sorting data by month. added writing new txt file

--- classes/checkClass.py
+++ classes/checkClass.py
@@ -16,5 +16,31 @@ class Check:
                 month = parts[-1].split('.')[0]
                 service = '_'.join(parts[:-1])
                 data.append([month, service])
+            self.df = pd.DataFrame(data, columns=['Месяц', 'Услуга'])
 
-        return pd.DataFrame(data, columns=['Месяц', 'Услуга'])
+            month_to_number = {
+                'январь': 1,
+                'февраль': 2,
+                'март': 3,
+                'апрель': 4,
+                'май': 5,
+                'июнь': 6,
+                'июль': 7,
+                'август': 8,
+                'сентябрь': 9,
+                'октябрь': 10,
+                'ноябрь': 11,
+                'декабрь': 12
+            }
+
+            self.df['Номера'] = self.df['Месяц'].map(month_to_number)
+            self.df = self.df.sort_values(by='Номера').reset_index(drop=True)
+            self.df.pop('Номера')
+        return self.df
+
+    def sorted_checks_to_file(self):
+        with open('new.txt', 'w', encoding='utf-8') as file:
+            for index, row in self.get_data().iterrows():
+                month = row['Месяц']
+                service = row['Услуга']
+                file.write(f"{month}/{service}_{month}.pdf\n")
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from classes.checkClass import Check
 file_path = 'чеки.txt'
 checks = Check(file_path)
 df = checks.get_data()
+checks.sorted_checks_to_file()
 
 print(df)
